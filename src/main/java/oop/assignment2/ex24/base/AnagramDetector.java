ef0a5fba02df8035d3277d9ed57c86dc78f41292
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-13 12:13:51 -0400

    Q24 Final commit

--- src/main/java/oop/assignment2/ex24/base/AnagramDetector.java
+++ src/main/java/oop/assignment2/ex24/base/AnagramDetector.java
@@ -8,17 +8,25 @@ import java.util.Arrays;
 public class AnagramDetector {
     public boolean isAnagram(String word1, String word2) {
 
+        //get length of strings
+        int n1 = word1.length();
+        int n2 = word2.length();
 
+        // If length of both strings is not same,
+        // then they cannot be anagram
+        if (n1 != n2)
+            return false;
 
-        // convert strings to character arrays
+        // string to character array
         char[] string1 = word1.toCharArray();
         char[] string2 = word2.toCharArray();
 
-        // sort character arrays
+        // sort characters
         Arrays.sort(string1);
         Arrays.sort(string2);
 
-        // compares sorted words and returns boolean value
-        return Arrays.equals(string1, string2);
+        // compared sorted check
+        Arrays.equals(string1, string2);
+            return true;
     }
 }
