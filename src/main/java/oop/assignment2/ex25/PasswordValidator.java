44d2d1b272782ee05404a26870dfd71427a56fc0
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-13 16:04:20 -0400

    Q25 PasswordValidator 1

--- src/main/java/oop/assignment2/ex25/PasswordValidator.java
+++ src/main/java/oop/assignment2/ex25/PasswordValidator.java
@@ -1,17 +1,42 @@
 package oop.assignment2.ex25;
 
-// fewer than 8 char and only number is very weak
-// fewer than 8 char and only letter is weak
-// at least 8 char and 1 numb is strong
-// at least 8 char, numbers, special char is very strong
+// fewer than 8 char and only number is very weak (no digits)
+// fewer than 8 char and only letter is weak (only digit, less than 8)
+// at least 8 char and 1 numb is strong (more than 8, no special)
+// at least 8 char, numbers, special char is very strong (more than 8, special (not a digit = does not equal?))
 
 public class PasswordValidator {
 
-    public boolean rankPassword(String password1) {
+    public int rankPassword(String password1) {
+
+        boolean gotNums;
+        boolean gotLetters;
+        boolean gotSpecials;
+        boolean passLength
 
         //check length
+        if(password1.length() < 8){
+            passLength = true;
+        }
 
-        //check characters
+        //set to array
+        char[] passArray = password1.toCharArray();
+        for (int i = 0; i < passArray.length; i++) {
 
+            //Digit check
+            if (Character.isDigit(passArray[i])) {
+                gotNums = true;
+            }
+
+            if (Character.isLetter(passArray[i])) {
+                gotLetters = true;
+            }
+
+            if (!Character.isDigit(passArray[i]) && !Character.isLetter(passArray[i])) {
+                gotSpecials = true;
+            }
+
+            //check characters
+        }
     }
 }
