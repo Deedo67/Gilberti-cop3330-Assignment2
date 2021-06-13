8094de74e60fe7cc2b1f8f40f983bec5b01ee97b
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-12 22:23:26 -0400

    Initial commit

---
+++ src/main/java/oop/assignment2/ex24/base/AnagramDetector.java
@@ -0,0 +1,7 @@
+package oop.assignment2.ex24.base;
+
+public class AnagramDetector {
+    public boolean isAnagram(String word1, String word2) {
+        return false;
+    }
+}
---
+++ src/main/java/oop/assignment2/ex24/base/solution24.java
@@ -0,0 +1,45 @@
+package oop.assignment2.ex24.base;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Derek Gilberti
+ */
+
+public class solution24 {
+    private static final Scanner in = new Scanner(System.in);
+
+    private static String word1;
+    private static String word2;
+
+    public static void main(String[] args) {
+        //reads two strings
+
+        AnagramDetector ad = new AnagramDetector();
+        boolean result = ad.isAnagram("alpha", "beta");
+
+        String output = generateOutput(result);
+
+        System.out.println(output);
+    }
+
+    public static void readUserInput() {
+        System.out.println("Enter word 1");
+        word1 = in.next();
+
+        System.out.println("Enter word 2");
+        word2 = in.next();
+    }
+
+    public static String generateOutput(boolean isAnagram) {
+        String output;
+        if(isAnagram) {
+            output = "is anagram";
+        } else {
+            output = "is not anagram";
+        }
+
+        return output;
+    }
+}
